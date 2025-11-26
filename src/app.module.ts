836973a6f58d36bcbfc494e4cd75093d26a45004
WebersonRodrigues7 <webersongiovani@gmail.com>
2025-11-25 21:59:48 -0300

    auth começado

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { CourseModule } from './course/course.module';
 import { EnrollmentModule } from './enrollment/enrollment.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { ConfigModule } from '@nestjs/config';
 }),
     CourseModule,
     EnrollmentModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
